42f0b7b5b59cb31bbd68e5808a659d236c3de195
llambduh <john@llambduh.com>
2024-05-27 22:45:12 -0400

    Added variables.py

---
+++ Variables/variables.py
@@ -0,0 +1,15 @@
+# Variables and assignment
+x = 5 # int: integer
+y = 3.59 # float
+name = "Llambduh" # str: string
+is_student = True # bool: boolean
+
+print("Value of x before conversion: ", x)
+print("Value of y before conversion: ", y)
+
+# Type conversions
+x = float(x)
+y = int(y)
+
+print("Value of x after conversion: ", x)
+print("Value of y after conversion: ", y)
