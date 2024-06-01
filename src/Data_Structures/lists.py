6657795632bbfff2aa6fcd7562f6bff0b78ee77c
llambduh <john@llambduh.com>
2024-06-01 04:12:02 -0400

    Updated lists.py

--- src/Data_Structures/lists.py
+++ src/Data_Structures/lists.py
@@ -29,6 +29,10 @@ print(squares)
 even_squares = [x**2 for x in range(1, 6) if x % 2 == 0]
 print(even_squares)
 
+# Enumerate adds a counter to an iterable and returns it as an enumerate object.
+for index, value in enumerate(squares):
+    print(index, value)
+
 
 # List Operations
 list1 = [1, 2, 3]
@@ -46,10 +50,20 @@ exists = 2 in list1  # Membership (in, not in): Checks if an element is present 
 doesnt_exists = 4 not in list1 
 
 
+# List Functions
+numbers = [1, 2, 3, 4, 5]
+length = len(numbers)  # len(iterable): Returns the length of an iterable
+minimum = min(numbers)  # min(iterable): Returns the smallest item in an iterable
+maximum = max(numbers)  # max(iterable): Returns the largest item in an iterable
+total = sum(numbers)  # sum(iterable): Returns the sum of all items in an iterable
+sorted_numbers = sorted(numbers)  # sorted(iterable, key=None, reverse=False): Returns a new sorted list from the items in iterable
+reversed_numbers = list(reversed(numbers))  # reversed(iterable): Returns an iterator that accesses the given sequence in reverse order
+
+
 # List Methods
-list1 = [1, 2, 3]
+list1 = list(1, 2, 3) # list(): Creates a new list
 idx = list1.index(2) # index(item): Returns the index of the first occurrence of an item.
-count = list1.count(2)  # count(item): Returns the number of occurrences of an item.
+list_count = list1.count(2)  # count(item): Returns the number of occurrences of an item.
 list1.append(4)  # append(item): Adds an item to the end of the list.
 list1.extend([4, 5, 6])  # extend(iterable): Extends the list by appending elements from an iterable.
 list1.insert(1, 1.5)  # insert(index, item): Inserts an item at a specified index.
@@ -61,4 +75,5 @@ list1.sort()  # sort(key=Function(optional), reverse=True/False(optional)): Defa
 list1.sort(key=len) # Sorting elements in the list by their length
 list1.sort(reverse=True)  # Sorting in Descending Order
 list1.sort(key=len, reverse=True) # Sorting by Length in Descending Order
+copy_list = list1.copy()  # copy(): Creates a shallow copy
 list1.clear() # clear(): Removes all items from the list.
