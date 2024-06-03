7e422b4623dfd09527ec1bc683b235340b95988e
llambduh <john@llambduh.com>
2024-06-03 10:44:07 -0400

    Updated lists.py

--- src/Data_Structures/lists.py
+++ src/Data_Structures/lists.py
@@ -6,10 +6,49 @@ Mutable - Items can be changed after creation.
 
 # Creating lists
 empty_list = []
-string_lists = ["A", "b", "cd", "EfG", "1"]
-numbers = [1, 2, 3, 4, 5]
-mixed = [1, "two", 3.0, True]
-matrix = [[1, 2, 3], [4, 5, 6], [7, 8, 9]]
+string_list = ["A", "b", "cd", "EfG", "1"]
+numbers_list = [1, 2, 3, 4, 5]
+mixed_list = [1, "two", 3.0, True]
+nested_list = [[1, 2, 3], [4, 5, 6], [7, 8, 9]] # Also called a matrix
+list1 = list(1, 2, 3) # list(): Creates a new list
+
+# Creating lists with String methods
+# split(separator(optional), maxsplit(optional)) - Breaks down a string into a list of substrings using a chosen separator
+name_with_spaces = "L l a m b d u h"
+fname = "John"
+maybe_you_could = "Like, Share, Subscribe"
+hashtags = "#youtube #new_video #python_course"
+chars_in_name = name_with_spaces.split() # Whitespace is used as the default delimiter
+fname_as_list = fname.split() # Splitting a string with no whitespace will return a new list of that one string
+itd_be_nice = maybe_you_could.split(',')
+hashtags_as_list = hashtags.split('#', 2)
+
+# rsplit([separator [, maxsplit]]) - Breaks the string at the separator starting from the right and returns a list of strings
+rchars_in_name = name_with_spaces.rsplit() # Whitespace is used as the default delimiter
+rfname_as_list = fname.rsplit() # Splitting a string with no whitespace will return a new list of that one string
+ritd_be_nice = maybe_you_could.rsplit(',')
+rhashtags_as_list = hashtags.rsplit('#', 2)
+
+# splitlines([keepends](optional)) - Splits the string at line breaks and returns a list
+sentence = "I\nlove\rThis\r\nChannel"
+grocery_list = '''Milk
+Chicken
+Bread
+Butter'''
+print(sentence.splitlines()) # splitlines works with 11 different escape sequences
+print(grocery_list.splitlines()) # splitlines also works on multi-line strings
+print(sentence.splitlines(True)) # The [keepends] param takes in either a boolean value or a number
+print(sentence.splitlines(0)) # True and 0 both evaluate to True
+print(sentence.splitlines(False)) # Any other positive or negative numbers indicate False
+print(sentence.splitlines(8))
+
+# List comprehension
+squares = [x ** 2 for x in range(10)]
+print(squares)
+
+# List comprehension with conditions
+even_squares = [x**2 for x in range(1, 6) if x % 2 == 0]
+print(even_squares)
 
 
 # Iterating over a list
@@ -20,14 +59,6 @@ for num in numbers:
 for row in matrix:
     for num in row:
         print(num)
-
-# List comprehension
-squares = [x ** 2 for x in range(10)]
-print(squares)
-
-# List comprehension with conditions
-even_squares = [x**2 for x in range(1, 6) if x % 2 == 0]
-print(even_squares)
 
 # Enumerate adds a counter to an iterable and returns it as an enumerate object.
 for index, value in enumerate(squares):
@@ -61,7 +92,7 @@ reversed_numbers = list(reversed(numbers))  # reversed(iterable): Returns an ite
 
 
 # List Methods
-list1 = list(1, 2, 3) # list(): Creates a new list
+list1 = list(1, 2, 3)
 idx = list1.index(2) # index(item): Returns the index of the first occurrence of an item.
 list_count = list1.count(2)  # count(item): Returns the number of occurrences of an item.
 list1.append(4)  # append(item): Adds an item to the end of the list.
